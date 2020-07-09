11a0d6d90a48f9bdabefa700e6ecf5124e5ca054
Schömer, Jörg <schoemer@adesso.de>
2020-07-09 09:29:44 +0200

    sort version to avoid differences in export

--- gradle-require-dependency-compliance-plugin/src/main/java/de/acetous/dependencycompliance/DependencyIdentifierComparator.java
+++ gradle-require-dependency-compliance-plugin/src/main/java/de/acetous/dependencycompliance/DependencyIdentifierComparator.java
@@ -1,6 +1,10 @@
 package de.acetous.dependencycompliance;
 
 import de.acetous.dependencycompliance.export.DependencyIdentifier;
+import org.gradle.api.internal.artifacts.ivyservice.ivyresolve.VersionInfo;
+import org.gradle.api.internal.artifacts.ivyservice.ivyresolve.Versioned;
+import org.gradle.api.internal.artifacts.ivyservice.ivyresolve.strategy.DefaultVersionComparator;
+import org.gradle.api.internal.artifacts.ivyservice.ivyresolve.strategy.VersionParser;
 
 import java.util.Comparator;
 
@@ -8,12 +12,23 @@ import java.util.Comparator;
  * Compares {@link DependencyIdentifier} by group, then name. Does not compare by version.
  */
 public class DependencyIdentifierComparator implements Comparator<DependencyIdentifier> {
+
+    private final DefaultVersionComparator defaultVersionComparator = new DefaultVersionComparator();
+    private final VersionParser parser = new VersionParser();
+
     @Override
     public int compare(DependencyIdentifier o1, DependencyIdentifier o2) {
         int comparedGroups = o1.getGroup().compareTo(o2.getGroup());
-        if (comparedGroups == 0) {
-            return o1.getName().compareTo(o2.getName());
+        if (comparedGroups != 0) {
+            return comparedGroups;
         }
-        return comparedGroups;
+        int compareNames = o1.getName().compareTo(o2.getName());
+        if (compareNames != 0) {
+            return compareNames;
+        }
+
+        Versioned v1 = new VersionInfo(parser.transform(o1.getVersion()));
+        Versioned v2 = new VersionInfo(parser.transform(o2.getVersion()));
+        return defaultVersionComparator.compare(v1, v2);
     }
 }
